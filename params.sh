e52953302b30cab7d72ee2b65154fb00c79e831d
TassSinclair <tass.aj@gmail.com>
2019-06-21 16:47:52 +1000

    Make the build fail

--- params.sh
+++ params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-EXIT_CODE=0 # 0 is pass, 1 is fail
-DELAY=20 # in seconds, max 595 (10 minute timeout)
+EXIT_CODE=1 # 0 is pass, 1 is fail
+DELAY=300 # in seconds, max 595 (10 minute timeout)
